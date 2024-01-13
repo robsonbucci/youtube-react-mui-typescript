4fc53ee09871b1c0c2420e4104f63086f8171061
robsonbucci <robsonbucci@hotmail.com>
2024-01-13 11:16:20 -0300

    feat: Menu lateral e lista

---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,54 @@
+import { Avatar, Box, Divider, Drawer, Icon, useTheme } from '@mui/material';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import React from 'react';
+const IMG_PERFIL =
+  'https://yt3.ggpht.com/X1OZQTM4R76JXkWy12eBXSKd6-uxBDAqndKAjzgJNFdZznvIj-SXIkl8TdpVIAqxl2Cn3MhpaA=s88-c-k-c0x00ffffff-no-rj';
+
+interface IMenuLateralProps {
+  children: React.ReactNode;
+}
+export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
+  const theme = useTheme();
+  return (
+    <>
+      <Drawer variant="permanent">
+        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center">
+            <Avatar
+              sx={{
+                height: theme.spacing(12),
+                width: theme.spacing(12),
+              }}
+              src={IMG_PERFIL}
+            />
+          </Box>
+
+          <Divider />
+
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página Inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
